5d43fc8132c397fa333e171798e03c4b4f0f7e4a
Túlio Cerqueira Lopes <58963175+tuliocl@users.noreply.github.com>
2023-08-27 17:30:44 -0300

    Create abalone.py

---
+++ abalone.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Atividade para trabalhar o pré-processamento dos dados.
+
+Criação de modelo preditivo para diabetes e envio para verificação de peformance
+no servidor.
+
+@author: Aydano Machado <aydano.machado@gmail.com>
+"""
+
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+import requests
+
+print('\n - Lendo o arquivo com o dataset sobre abalones')
+data = pd.read_csv('abalone_dataset.csv')
+
+# Criando X and y par ao algorítmo de aprendizagem de máquina.\
+print(' - Criando X e y para o algoritmo de aprendizagem a partir do arquivo abalone_dataset')
+# Caso queira modificar as colunas consideradas basta algera o array a seguir.
+features = ['sex','length', 'diameter', 'height', 'whole_weight', 'shucked_weight', 'viscera_weight', 'shell_weight','type']
+X = data.drop('type', axis=1)
+y = data['type']
+
+# Ciando o modelo preditivo para a base trabalhada
+print(' - Criando modelo preditivo')
+neigh = KNeighborsClassifier(n_neighbors=15)
+neigh.fit(X, y)
+
+#realizando previsões com o arquivo de
+print(' - Aplicando modelo e enviando para o servidor')
+features = ['sex','length', 'diameter', 'height', 'whole_weight', 'shucked_weight', 'viscera_weight', 'shell_weight']
+data_app = pd.read_csv('abalone_app.csv')
+data_app = data_app[features]
+y_pred = neigh.predict(data_app)
+
+# Enviando previsões realizadas com o modelo para o servidor
+URL = "https://aydanomachado.com/mlclass/03_Validation.php"
+
+#TODO Substituir pela sua chave aqui
+DEV_KEY = "Trio parada mole"
+
+# json para ser enviado para o servidor
+data = {'dev_key':DEV_KEY,
+        'predictions':pd.Series(y_pred).to_json(orient='values')}
+
+# Enviando requisição e salvando o objeto resposta
+r = requests.post(url = URL, data = data)
+
+# Extraindo e imprimindo o texto da resposta
+pastebin_url = r.text
+print(" - Resposta do servidor:\n", r.text, "\n")
